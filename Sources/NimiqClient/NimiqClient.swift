1cf2b401a5730430031e56aa4726e62e419f8b7c
Developer <>
2020-08-04 17:59:53 -0400

    subclass VestingCrontract and HTLC from Account and improve example

--- Sources/NimiqClient/NimiqClient.swift
+++ Sources/NimiqClient/NimiqClient.swift
@@ -27,7 +27,7 @@ public enum AccountType: Int, Decodable {
 }
 
 /// Normal Nimiq account object returned by the server.
-public struct Account: Decodable {
+public class Account: Decodable {
     /// Hex-encoded 20 byte address.
     public var id: String
     /// User friendly address (NQ-address).
@@ -39,15 +39,7 @@ public struct Account: Decodable {
 }
 
 /// Vesting contract object returned by the server.
-public struct VestingContract : Decodable {
-    /// Hex-encoded 20 byte address.
-    public var id: String
-    /// User friendly address (NQ-address).
-    public var address: String
-    /// Balance of the account (in smallest unit).
-    public var balance: Int
-    /// The account type associated with the account.
-    public var type: AccountType
+public class VestingContract : Account {
     /// Hex-encoded 20 byte address of the owner of the vesting contract.
     public var owner: String
     /// User friendly address (NQ-address) of the owner of the vesting contract.
@@ -60,18 +52,25 @@ public struct VestingContract : Decodable {
     public var vestingStepAmount: Int
     /// The total amount (in smallest unit) that was provided at the contract creation.
     public var vestingTotalAmount: Int
+    
+    private enum CodingKeys: String, CodingKey {
+        case owner, ownerAddress, vestingStart, vestingStepBlocks, vestingStepAmount, vestingTotalAmount
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.owner = try container.decode(String.self, forKey: .owner)
+        self.ownerAddress = try container.decode(String.self, forKey: .ownerAddress)
+        self.vestingStart = try container.decode(Int.self, forKey: .vestingStart)
+        self.vestingStepBlocks = try container.decode(Int.self, forKey: .vestingStepBlocks)
+        self.vestingStepAmount = try container.decode(Int.self, forKey: .vestingStepAmount)
+        self.vestingTotalAmount = try container.decode(Int.self, forKey: .vestingTotalAmount)
+        try super.init(from: decoder)
+    }
 }
 
 /// Hashed Timelock Contract object returned by the server.
-public struct HTLC : Decodable {
-    /// Hex-encoded 20 byte address.
-    public var id: String
-    /// User friendly address (NQ-address).
-    public var address: String
-    /// Balance of the account (in smallest unit).
-    public var balance: Int
-    /// The account type associated with the account.
-    public var type: AccountType
+public class HTLC : Account {
     /// Hex-encoded 20 byte address of the sender of the HTLC.
     public var sender: String
     /// User friendly address (NQ-address) of the sender of the HTLC.
@@ -90,6 +89,24 @@ public struct HTLC : Decodable {
     public var timeout: Int
     /// The total amount (in smallest unit) that was provided at the contract creation.
     public var totalAmount: Int
+    
+    private enum CodingKeys: String, CodingKey {
+        case sender, senderAddress, recipient, recipientAddress, hashRoot, hashAlgorithm, hashCount, timeout, totalAmount
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.sender = try container.decode(String.self, forKey: .sender)
+        self.senderAddress = try container.decode(String.self, forKey: .senderAddress)
+        self.recipient = try container.decode(String.self, forKey: .recipient)
+        self.recipientAddress = try container.decode(String.self, forKey: .recipientAddress)
+        self.hashRoot = try container.decode(String.self, forKey: .hashRoot)
+        self.hashAlgorithm = try container.decode(Int.self, forKey: .hashAlgorithm)
+        self.hashCount = try container.decode(Int.self, forKey: .hashCount)
+        self.timeout = try container.decode(Int.self, forKey: .timeout)
+        self.totalAmount = try container.decode(Int.self, forKey: .totalAmount)
+        try super.init(from: decoder)
+    }
 }
 
 /// Nimiq account returned by the server. The especific type is in the associated value.
